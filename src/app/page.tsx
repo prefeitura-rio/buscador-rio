69813a5553fda859e4f533366958b4a37a310966
Lucas Tavares <lucastavarestt@gmail.com>
2025-03-14 15:23:33 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,120 @@
-import Image from "next/image";
+'use client'
+import { useState } from 'react';
+import { XIcon, ArrowRightIcon, ExternalLinkIcon } from 'lucide-react';
+
+type SearchResultItem = {
+  titulo: string;
+  descricao: string;
+  link_acesso?: string;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<SearchResultItem[]>([]);
+  const [loading, setLoading] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+
+    if (e.target.value.length > 2) {
+      setLoading(true);
+      try {
+        const response = await fetch(`/api/search?q=${e.target.value}`);
+        const data = await response.json();
+        setResults(data.result || []);
+        console.log('Search results:', data.result);
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+        setResults([]);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      setResults([]);
+    }
+  };
+
+  const clearSearch = () => {
+    setQuery('');
+    setResults([]);
+  };
+
+  const handleAdvancedSearch = () => {
+    window.open(`https://prefeitura.rio/buscaavancada?q=${encodeURIComponent(query)}`, '_blank');
+  };
+
+  return (
+    <div className="relative">
+      {/* Fixed Header */}
+      <div className="fixed top-0 left-0 w-full bg-cover bg-center z-50" style={{ backgroundImage: "url('./header.png')", height: '220px' }}>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white mx-2">
+          <h1 className="text-xl mx-10 md:text-3xl font-bold mb-4 text-center">A Prefeitura do Rio mais perto de você</h1>
+          <div className="relative w-full max-w-md">
+            {/* SearchBox */}
+            <input
+              type="text"
+              value={query}
+              onChange={handleSearch}
+              className="w-full p-3 border text-black border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#12bbef] pr-10"
+              placeholder="O que você procura?"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {/* X Icon to Clear Search */}
+            {query.length > 0 && (
+              <button
+                onClick={clearSearch}
+                className="absolute inset-y-0 right-3 flex items-center"
+              >
+                <XIcon className="h-5 w-5 text-gray-400 hover:text-gray-600 transition" style={{ width: '20px', height: '20px' }} />
+              </button>
+            )}
+
+            {/* Results Box */}
+            {(query.length > 2 || loading) && (
+              <div className="absolute w-full mt-2 bg-white text-black rounded-md shadow-lg">
+                <ul className="divide-y divide-gray-200 max-h-72 overflow-y-auto">
+                  {loading ? (
+                    <div className="flex justify-center items-center py-2">
+                      <div className="loader ease-linear rounded-full border-t-4 border-gray-200 h-6 w-6"></div>
+                    </div>
+                  ) : (
+                    results.map((item, index) => (
+                      <li
+                        key={index}
+                        className="p-3 hover:bg-gray-100 cursor-pointer flex justify-between items-center"
+                        onClick={() => {
+                          if (item.link_acesso) {
+                            window.open(item.link_acesso, '_blank');
+                          }
+                        }}
+                      >
+                        <div className="mr-4">
+                          <strong>{item.titulo}</strong>
+                          <p className='text-sm text-gray-500'>
+                            {item.descricao.length > 50 ? `${item.descricao.substring(0, 50)}...` : item.descricao}
+                          </p>
+                        </div>
+                        <div style={{ width: '20px', height: '20px' }}>
+                          <ExternalLinkIcon className="h-5 w-5 text-gray-400 hover:text-gray-600 transition" />
+                        </div>
+                      </li>
+                    ))
+                  )}
+                </ul>
+
+                {/* Fixed Bottom Button */}
+                <button
+                  onClick={handleAdvancedSearch}
+                  className="w-full cursor-pointer bg-[#12bbef] text-white p-3 text-center rounded-b-md font-medium hover:bg-black hover:text-white transition-all duration-300 flex justify-between items-center"
+                >
+                  <span>Busca Avançada</span>
+                  <ArrowRightIcon className="h-5 w-5" style={{ width: '20px', height: '20px' }} />
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
