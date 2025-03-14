4722f7277d9bc8261253b4b515ba970481d63558
Lucas Tavares <lucastavarestt@gmail.com>
2025-03-14 16:56:17 -0300

    style progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,119 +1,60 @@
 'use client'
 import { useState } from 'react';
-import { XIcon, ArrowRightIcon, ExternalLinkIcon } from 'lucide-react';
-
-type SearchResultItem = {
-  titulo: string;
-  descricao: string;
-  link_acesso?: string;
-};
+import { ArrowRightIcon, Plus, Image as ImageIcon, Mic } from 'lucide-react';
+import Image from 'next/image';
+import "./globals.css"
 
 export default function Home() {
   const [query, setQuery] = useState('');
-  const [results, setResults] = useState<SearchResultItem[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-
-    if (e.target.value.length > 2) {
-      setLoading(true);
-      try {
-        const response = await fetch(`/api/search?q=${e.target.value}`);
-        const data = await response.json();
-        setResults(data.result || []);
-        console.log('Search results:', data.result);
-      } catch (error) {
-        console.error('Error fetching search results:', error);
-        setResults([]);
-      } finally {
-        setLoading(false);
-      }
-    } else {
-      setResults([]);
-    }
-  };
-
-  const clearSearch = () => {
-    setQuery('');
-    setResults([]);
-  };
-
-  const handleAdvancedSearch = () => {
-    window.open(`https://prefeitura.rio/buscaavancada?q=${encodeURIComponent(query)}`, '_blank');
-  };
 
   return (
-    <div className="relative">
-      {/* Fixed Header */}
-      <div className="fixed top-0 left-0 w-full bg-cover bg-center z-50" style={{ backgroundImage: "url('./header.png')", height: '220px' }}>
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="relative z-10 flex flex-col items-center justify-center h-full text-white mx-2">
-          <h1 className="text-xl mx-10 md:text-3xl font-bold mb-4 text-center">A Prefeitura do Rio mais perto de você</h1>
-          <div className="relative w-full max-w-md">
-            {/* SearchBox */}
-            <input
-              type="text"
-              value={query}
-              onChange={handleSearch}
-              className="w-full p-3 border text-black border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-[#12bbef] pr-10"
-              placeholder="O que você procura?"
-            />
-            {/* X Icon to Clear Search */}
-            {query.length > 0 && (
-              <button
-                onClick={clearSearch}
-                className="absolute inset-y-0 right-3 flex items-center"
-              >
-                <XIcon className="h-5 w-5 text-gray-400 hover:text-gray-600 transition" style={{ width: '20px', height: '20px' }} />
-              </button>
-            )}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#F8F8F8] relative">
 
-            {/* Results Box */}
-            {(query.length > 2 || loading) && (
-              <div className="absolute w-full mt-2 bg-white text-black rounded-md shadow-lg">
-                <ul className="divide-y divide-gray-200 max-h-72 overflow-y-auto">
-                  {loading ? (
-                    <div className="flex justify-center items-center py-2">
-                      <div className="loader ease-linear rounded-full border-t-4 border-gray-200 h-6 w-6"></div>
-                    </div>
-                  ) : (
-                    results.map((item, index) => (
-                      <li
-                        key={index}
-                        className="p-3 hover:bg-gray-100 cursor-pointer flex justify-between items-center"
-                        onClick={() => {
-                          if (item.link_acesso) {
-                            window.open(item.link_acesso, '_blank');
-                          }
-                        }}
-                      >
-                        <div className="mr-4">
-                          <strong>{item.titulo}</strong>
-                          <p className='text-sm text-gray-500'>
-                            {item.descricao.length > 50 ? `${item.descricao.substring(0, 50)}...` : item.descricao}
-                          </p>
-                        </div>
-                        <div style={{ width: '20px', height: '20px' }}>
-                          <ExternalLinkIcon className="h-5 w-5 text-gray-400 hover:text-gray-600 transition" />
-                        </div>
-                      </li>
-                    ))
-                  )}
-                </ul>
+      {/* Logo da Prefeitura */}
+      <div className="absolute top-10 flex items-center gap-2">
+        <Image src="/logo_prefeitura.svg" alt="Prefeitura do Rio" width={80} height={100} className="mb-6" />
+      </div>
 
-                {/* Fixed Bottom Button */}
-                <button
-                  onClick={handleAdvancedSearch}
-                  className="w-full cursor-pointer bg-[#12bbef] text-white p-3 text-center rounded-b-md font-medium hover:bg-black hover:text-white transition-all duration-300 flex justify-between items-center"
-                >
-                  <span>Busca Avançada</span>
-                  <ArrowRightIcon className="h-5 w-5" style={{ width: '20px', height: '20px' }} />
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
+      {/* Título */}
+      <h1 className="text-4xl sm:text-5xl font-semibold text-center bg-gradient-to-r from-[#27B8DB] to-[#3F38AC] bg-clip-text text-transparent">
+        Fale com a gente! <br /> Como podemos ajudar?
+      </h1>
+
+      {/* Campo de pesquisa */}
+      <div className="relative mt-6 w-full max-w-md sm:max-w-3xl">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="w-full p-4 pl-6 pr-14  bg-white rounded-full focus:outline-none  text-gray-700 text-lg"
+          placeholder="O que você está precisando?"
+        />
+        <button className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-600">
+          <ArrowRightIcon size={24} />
+        </button>
+      </div>
+
+      {/* Botões de adicionar arquivos, imagem e áudio */}
+      <div className="flex gap-4 mt-4">
+        <button className="flex flex-row items-center gap-2 px-4 py-2 rounded-full text-gray-400 bg-white hover:bg-gray-100">
+          <Plus size={20} /> Adicionar arquivos
+        </button>
+        <button className="flex flex-row items-center gap-2 px-4 py-2 rounded-full text-gray-400 bg-white hover:bg-gray-100">
+          <ImageIcon size={20} /> Adicionar Imagem
+        </button>
+        <button className="flex flex-row items-center gap-2 px-4 py-2 rounded-full text-gray-400 bg-white hover:bg-gray-100">
+          <Mic size={20} /> Enviar áudio
+        </button>
+      </div>
+
+      {/* Botão principal */}
+      <button className="mt-10 px-5 py-2 text-white rounded-full text-md bg-linear-to-r/srgb from-[#27B8DB] to-[#3F38AC]">
+        Preciso de ajuda
+      </button>
+
+      {/* Logo IplanRio */}
+      <div className="absolute bottom-10 flex items-center gap-2">
+        <Image src="/logo_iplan.svg" alt="Iplan logo" width={110} height={110} className="mb-6" />
       </div>
     </div>
   );
