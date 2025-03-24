76c2d24c513c32916466895358b240599f722504
Lucas Tavares <lucastavarestt@gmail.com>
2025-03-24 12:16:14 -0300

    Revert "fix"
    This reverts commit 6fe52b3e00b7b2517e73b2b35199da1eb9ea6808.

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { q } = req.query;
-  const token = process.env.NEXT_TYPESENSE_API_KEY;
+  const token = process.env.NEXT_PUBLIC_TYPESENSE_API_KEY;
   const headers = { Authorization: `Bearer ${token}` };
 
   try {
