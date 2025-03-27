99fc7c59472bce63286ae0d45c5fdfde197044ad
Lucas Tavares <lucastavarestt@gmail.com>
2025-03-27 19:02:37 -0300

    .

--- src/utils/tipos.ts
+++ src/utils/tipos.ts
@@ -1,4 +1,3 @@
-// Add a function or inline logic to map the 'tipo' to the desired display value
 export const displayTipo = (tipo: string | undefined) => {
   switch (tipo) {
     case "noticia":
@@ -8,6 +7,6 @@ export const displayTipo = (tipo: string | undefined) => {
     case "servico":
       return "Serviço";
     default:
-      return tipo; // Fallback to the original value if no match
+      return tipo;
   }
 };
