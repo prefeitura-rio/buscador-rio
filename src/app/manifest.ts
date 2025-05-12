9558029f3b4f83f1915db7a6e8b6c0077ed0298b
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2025-05-12 12:37:32 -0300

    feat: PWA config

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Buscador Rio",
+    short_name: "Buscador",
+    description: "Fale com a gente! Como podemos ajudar?",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/logo_iplan.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/logo_iplan.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
